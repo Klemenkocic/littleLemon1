4edd64a440d8449e6d4213078621dd904492321f
Klemen Kocic <Klemen@seascape.network>
2023-12-20 20:47:18 +0100

    update with API (Just added a bit to make sure reservation/menu/ works

--- reservation/urls.py
+++ reservation/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('menu/items', views.MenuItemView.as_view(), name='menu-items-list'),  # Adjusted path to match your URL
     path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='menu-detail'),
+    #path('menu/', views.MenuItemView.as_view(), name='menu-list'),  # Add this line for 'reservation/menu/'
+
 ]
